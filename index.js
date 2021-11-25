5a744febb2316413c5c911a130b933f2ff981f1d
David Guerin <david@devitgo.com>
2021-11-25 19:08:27 -0300

    fix: fixed the app

--- index.js
+++ index.js
@@ -2,15 +2,8 @@
  * @format
  */
 
-import { CacheProvider } from 'rest-hooks';
-import React, { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./src/App";
+import { name as appName } from "./app.json";
 
-const Root = () => (
-    <CacheProvider>
-        <App />
-    </CacheProvider>
-);
-
-AppRegistry.registerComponent(appName, () => Root);
+AppRegistry.registerComponent(appName, () => App);
